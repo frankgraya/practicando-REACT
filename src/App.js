c88c6d023d0bdba33acb35e5ffe034e47252ee04
Frank graya <francisco.graya26@gmail.com>
2021-07-25 23:37:55 -0500

    se agrego vamos aprender react

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vamos aprender REACT 
         </a>
       </header>
     </div>
